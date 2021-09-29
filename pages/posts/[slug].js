00e882cf761d8bb1c905e3d0e35750960a251450
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-09-29 17:41:17 -0300

    189: rendered dynamic post pages & paths

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,9 +1,34 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../utils/posts-util";
 
-const PostDetails = () => {
+const PostDetails = ({ post }) => {
     return (
-        <PostContent />
+        <PostContent post={post} />
     );
 };
+
+export function getStaticProps(context) {
+    const { params } = context;
+    const { slug } = params;
+
+    const postData = getPostData(slug);
+
+    return {
+        props: {
+            post: postData
+        },
+        revalidate: 600
+    };
+};
+
+export function getStaticPaths() {
+    const postFiles = getPostsFiles();
+
+    const slugs = postFiles.map(fileName => fileName.replace(/\.md$/, ''))
+    return {
+        paths: slugs.map(slug => ({ params: { slug: slug }})),
+        fallback: false
+    }
+}
 
 export default PostDetails;
--- utils/posts-util.js
+++ utils/posts-util.js
@@ -5,12 +5,15 @@ import matter from 'gray-matter';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
-const getPostData = (fileName) => {
-    const filePath = path.join(postsDirectory, fileName);
+export const getPostsFiles = () => {
+    return fs.readdirSync(postsDirectory);
+}
+
+export const getPostData = (postIdentifier) => {
+    const postSlug = postIdentifier.replace(/\.md$/, ''); 
+    const filePath = path.join(postsDirectory, `${postSlug}.md`);
     const fileContent = fs.readFileSync(filePath, 'utf-8');
     const { data, content } = matter(fileContent);
-
-    const postSlug = fileName.replace(/\.md$/, ''); 
 
     const postData = {
         slug: postSlug,
@@ -22,7 +25,7 @@ const getPostData = (fileName) => {
 };
 
 export const getAllPosts = () => {
-    const postFiles = fs.readdirSync(postsDirectory);
+    const postFiles = getPostsFiles();
 
     const allPosts = postFiles.map(postFile => {
         return getPostData(postFile)
